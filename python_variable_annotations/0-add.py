f782c7ab86baf706f4b64df2f528378873a2ec41
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-26 00:07:44 +0100

    :sparkles: feat: Add type-annotated function that sum two floats

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+def add(a: float, b: float):
+    """
+    Sum two floats.
+    
+    Args:
+        a (float): The first float to add.
+        b (float): The second float to add.
+    """
+    return a + b
