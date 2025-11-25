585f22210d4a432c57349cff9821e040d31a5f75
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-26 00:39:04 +0100

    :bug: fix: Add module docstring and pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+"""
+Module 0-add
+"""
 
-def add(a: float, b: float):
+
+def add(a: float, b: float) -> float:
     """
     Sum two floats.
-    
+
     Args:
         a (float): The first float to add.
         b (float): The second float to add.
