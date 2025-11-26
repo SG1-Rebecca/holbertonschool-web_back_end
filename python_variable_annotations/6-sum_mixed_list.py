01687b8be151e2c936f44bcb874a0dde789d516b
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-26 02:28:03 +0100

    :sparkles: feat: Sum mixed list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+Module 6-sum_mixed_list
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Sum a list of integers and floats
+
+    Args:
+        mxd_lst (List[Union[int, float]]): List of integers and floats
+
+    Returns:
+        float: The sum of the list
+    """
+    return sum(mxd_lst)
