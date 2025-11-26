cf03e8da4b96fbe53eb766c458e5b0f740d6851a
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-26 02:47:43 +0100

    :bug: fix: Return type

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """
 Module 7-to_kv
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a formatted tuple
 
