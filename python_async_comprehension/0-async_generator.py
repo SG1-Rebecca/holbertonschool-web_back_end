03bae97bbbb7cbbf2c6574f1babe389739485183
Rebecca F <11508@holbertonstudents.com>
2026-04-12 23:00:02 +0200

    :sparkles: feat: async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+0-async_generator.py
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    An async generator that yields random numbers between 0 and 10.
+
+    Args:
+        None
+
+    Returns:
+        Generator[float, None, None]: An asynchronous generator that yields
+        random numbers between 0 and 10 every second for 10 seconds.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
