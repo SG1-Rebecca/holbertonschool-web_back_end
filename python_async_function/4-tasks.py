8c47c555d3e8a8e406d227727c3e89f919902ecc
Rebecca F <11508@holbertonstudents.com>
2026-04-12 18:25:07 +0200

    :bug: fix task error

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,11 +3,12 @@
 Module 4-tasks
 """
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns wait_random n times with the specified max_delay
 
