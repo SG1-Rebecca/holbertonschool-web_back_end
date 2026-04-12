71d0d0e2518514468eeb2798d2ab84af1bbf1574
Rebecca F <11508@holbertonstudents.com>
2026-04-12 17:18:16 +0200

    :sparkles: feat(concurrent-coroutines): execute multiple coroutines at the same time

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module 1-concurrent_coroutines
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous coroutine that spawns wait_random n times
+
+    Args:
+        n (int): Number of times to spawn wait_random
+        max_delay (int): Maximum delay in seconds for wait_random
+    Returns:
+        List[float]: List of all the delays.
+    """
+    delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
