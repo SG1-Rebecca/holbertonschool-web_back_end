212b9d741be8b4620d1e258c7f73fa48cd8b75ca
Rebecca F <11508@holbertonstudents.com>
2026-04-24 23:54:42 +0200

    :bug: fix(pymongo): add PATCH method and __main__ guard

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -10,7 +10,7 @@ def main():
     """
     Provide some stats about Nginx logs stored in MongoDB
     """
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient()
     collection = client.logs.nginx
 
     # count and print logs
@@ -19,11 +19,14 @@ def main():
 
     # print methods
     print('Methods:')
-    methods = ["GET", "POST", "PUT", "DELETE"]
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f'method {method}: {count}')
+        print(f'\tmethod {method}: {count}')
 
     # count and print status check
     status_count = collection.count_documents({"method": "GET", "path": "/status"})
     print(f'{status_count} status check')
+
+if __name__ == "__main__":
+    main()
